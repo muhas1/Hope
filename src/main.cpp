4ac03fca3fd53fabffa83dedd986b4725ff51514
Muhammad Shah <muhammad.shah@ucalgary.ca>
2025-08-03 22:55:01 -0600

    Initially loading Barbarian, doesn't work because no 3D Space for Model

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ int main ()
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
+	Model Barbarian = LoadModel("Barbarian.glb");
 
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
@@ -38,13 +39,16 @@ int main ()
 		DrawText("Hope Game Initial Conditions", 200,200,20,WHITE);
 
 		// draw our texture to the screen
-		
+		DrawModel(Barbarian, {0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
+
+
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
 
 	// cleanup
 	// unload our texture so it can be cleaned up
+	UnloadModel(Barbarian);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
